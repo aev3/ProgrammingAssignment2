88c9950a07e4aa1d263b25ac55c1e43622b7fe89
rtv3 <art@AWOLart.com>
2014-05-21 08:45:30 -0400

    Updated cacher to reflect consensus approach; i.e., does not use R.cache, which usage is, imho, more elegant.

--- cachematrix.R
+++ cachematrix.R
@@ -1,98 +1,31 @@
-#
-#
-# Introduction
-# 
-# Write an R function that is able to cache potentially time-consuming 
-# computations. For example, taking the mean of a numeric vector is 
-# typically a fast operation. However, for a very long vector, it may 
-# take too long to compute the mean, especially if it has to be 
-# computed repeatedly (e.g. in a loop). If the contents of a vector are 
-# not changing, it may make sense to cache the value of the mean so that 
-# when we need it again, it can be looked up in the cache rather than 
-# recomputed. 
-# 
-# Take advantage of the scoping rules of the R language and how they can 
-# be manipulated to preserve state inside of an R object.
-#
-# Example: Caching the Mean of a Vector
-# 
-# In this example we introduce the '<<-' operator which can be used 
-# to assign a value to an object in an environment that is different 
-# from the current environment. Below are two functions that are 
-# used to create a special object that stores a numeric vector and 
-# cache's its mean.
-# 
-# The first function, makeVector creates a special "vector", which is 
-# really a list containing a function to:
-# set the value of the vector
-# get the value of the vector
-# set the value of the mean
-# get the value of the mean
-#
+## Put comments here that give an overall description of what your
+## functions do
 
-#Examples
-simulate <- function(mean, sd) {
-  # 1. Try to load cached data, if already generated
-  key <- list(mean, sd)
-  data <- loadCache(key)
-  if (!is.null(data)) {
-    cat("Loaded cached data\n")
-    return(data);
-  }
-  # 2. If not available, generate it.
-  cat("Generating data from scratch ... ")
-  data <- rnorm(1000, mean=mean, sd=sd)
-  Sys.sleep(1) # Emulate slow algorithm
-  cat("ok\n")
-  saveCache(data, key=key, comment="simulate()")
-  data;
-}
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-#   m <- NULL
-#   set <- function(y) {
-#     x <<- y
-#     m <<- NULL
-#   }
-#   get <- function() x
-#   setmean <- function(mean) m <<- mean
-#   getmean <- function() m
-#   list(set = set, get = get,
-#        setmean = setmean,
-#        getmean = getmean)
-    key <- "http://mytext.com"
-    value <- "This is the content I downloaded"
-    cacheEnv <- new.env()
-    assign(key, value, envir=cacheEnv)
-    get(key, envir=cacheEnv)
-}
-#
-# The following function calculates the mean of the special "vector" 
-# created with the above function. However, it first checks to see 
-# if the mean has already been calculated. If so, it gets the mean from 
-# the cache and skips the computation. Otherwise, it calculates the mean
-# of the data and sets the value of the mean in the cache via the 
-# setmean function.
-#
-cacheSolve <- function(x, ...) {
-#   m <- NULL
-#   set <- function(y) {
-#     x <<- y
-#     m <<- NULL
-#   }
-#   get <- function() x
-#   setmean <- function(mean) m <<- mean
-#   getmean <- function() m
-#   list(set = set, get = get,
-#        setmean = setmean,
-#        getmean = getmean)
-#       ## Return a matrix that is the inverse of 'x'
-    if (exists(url, envir=cacheEnv)) {
-      return(get(url, envir=cacheEnv))
-    }
-    
-    file <- content(GET(url))
-    assign(url, file, envir=cacheEnv)
-    
-    file
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
   }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
